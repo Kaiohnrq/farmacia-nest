be4eda6539d0d54b48a8412da6ff9049adf86f48
Kaio <kaio.gunz@gmail.com>
2022-09-13 17:04:41 -0300

    atualizado

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   process.env.TZ="-03:00"
-  
   app.useGlobalPipes(new ValidationPipe())
-  
   app.enableCors()
 
   await app.listen(3000);
